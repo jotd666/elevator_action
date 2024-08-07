85531ee4cab9be0c952b6545990b948cb01975c3
Jean-François Fabre <jotd666@gmail.com>
2024-08-07 22:33:43 +0200

    convert graphics started

--- assets/amiga/convert_graphics_aga.py
+++ assets/amiga/convert_graphics_aga.py
@@ -1,3 +1,8 @@
+# TODO:
+# - transparent color that is not black (for spies)
+# - identify dynamic colors per level (4 sets)
+# - identify which colors to change when lights are shot
+
 from PIL import Image,ImageOps
 import os,bitplanelib
 
@@ -21,6 +26,10 @@ this_dir = os.path.dirname(os.path.abspath(__file__))
 
 side = 8
 transparent = (254,254,254)  # not possible to get it in the game
+dummy = (1,1,1)  # not possible to get it in the game
+
+def dump_asm_bytes(*args,**kwargs):
+    bitplanelib.dump_asm_bytes(*args,**kwargs,mit_format=True)
 
 # for each layer, very few tiles are used. Don't generate all tiles with the given colors,
 # just the ones that are used
@@ -33,12 +42,14 @@ used_game_tiles = {"status":set(range(16,52)),"building":set(range(64,94)) | {14
   169,
   197},"elevators":{252, 55, 56, 58, 59, 60, 61, 62, 63}}
 
-used_title_tiles = {"status":set(range(16,52)),
+used_title_tiles = {"status":set(range(16,50)) | {51},
 "big_letters": set(range(80,128)) | {158} | set(range(181,256)),
 "elevator_letters": set(range(128,181)) | {248, 249}}
 
 game_layer_names = ["status","building","elevators"]
 title_layer_names = ["status","big_letters","elevator_letters"]
+
+tile_bitplane_cache = dict()
 
 tile_palettes = []
 game_palettes = []
@@ -69,6 +80,8 @@ def load_tileset(image_name,game_gfx,side,used_tiles,tileset_name,dump=False):
         ensure_empty(dump_subdir)
 
     tile_number = 0
+    palette = set()
+
     for j in range(nb_rows):
         for i in range(nb_cols):
             tile_number += 1
@@ -78,13 +91,16 @@ def load_tileset(image_name,game_gfx,side,used_tiles,tileset_name,dump=False):
 
             img = Image.new("RGBA",(side,side))
             img.paste(tiles_1,(-i*side,-j*side))
+
+            # only consider colors of used tiles
+            palette.update(set(bitplanelib.palette_extract(img)))
             tileset_1.append(img)
             if dump:
                 img = ImageOps.scale(img,5,resample=Image.Resampling.NEAREST)
                 img.save(os.path.join(dump_subdir,f"{k:02x}.png"))
             k += 1
 
-    palette = bitplanelib.palette_extract(tiles_1)
+
     return sorted(set(palette)),tileset_1
 
 dump_it = True
@@ -96,9 +112,95 @@ sprites_palette,sprites_set = load_tileset("sprites_4.png",True,16,None,"sprites
 
 # sprites can be on either playfield.
 
-playfield_1_palette = game_layer[1][0]
-playfield_1_palette = game_layer[1][0]
+game_playfield_palette = tuple(sorted(set(game_layer[1][0]+sprites_palette)))
+game_playfield_palette = game_playfield_palette + (16-len(game_playfield_palette)) * (dummy,)
 
 title_layer = [load_tileset("tiles_{}.png".format(i if i<2 else 1),False,side,used_title_tiles[layer_name],layer_name,dump=dump_it) for i,layer_name in enumerate(title_layer_names)]
+
+title_playfield_palette = tuple(sorted(set(title_layer[1][0])))
+title_playfield_palette = title_playfield_palette + (16-len(title_playfield_palette)) * (dummy,)
+
+current_plane_idx = 0
+
+layer_bitmaps = {}
+for tn,tc,palette in (["title",title_layer,title_playfield_palette],["game",game_layer,game_playfield_palette]):
+    layer_bitmaps[tn] = [[] for _ in range(3)]
+    for lidx,(_,ts) in enumerate(tc):  # for each layer
+        tile_list = layer_bitmaps[tn][lidx]
+
+        for tidx,tile in enumerate(ts):  # for each tile
+            if tile:
+                try:
+                    planes = bitplanelib.palette_image2raw(tile,None,palette,forced_nb_planes=4)
+                except bitplanelib.BitplaneException as e:
+                    print(tn,lidx,tidx,e)
+                    tile_list.append(None)
+                    continue
+
+                nb_planes = 4
+                plane_list = []
+                planesize = len(planes)//nb_planes
+                for sl in range(0,len(planes),planesize):
+                    plane = planes[sl:sl+planesize]
+                    if any(plane):
+                        plane_idx = tile_bitplane_cache.get(plane)
+                        if plane_idx is None:
+                            # create entry
+                            plane_idx = current_plane_idx
+                            tile_bitplane_cache[plane] = current_plane_idx
+                            current_plane_idx += 1
+                        plane_list.append(plane_idx)
+                    else:
+                        # empty plane: use index -1
+                        plane_list.append(-1)
+                tile_list.append(plane_list)
+            else:
+                tile_list.append(None)
+
+
+src_dir = os.path.join(this_dir,os.pardir,os.pardir,"src","amiga")
+
+with open(os.path.join(src_dir,"graphics.68k"),"w") as f:
+    f.write("\t.global\tcharacter_tables\n")
+    f.write("\t.global\tbob_table\n")   # BOBs only present in game
+
+    f.write("""
+character_tables:
+\t.long\tchar_table_title
+\t.long\tchar_table_game
+""")
+
+    for tt,lb in layer_bitmaps.items():
+        f.write(f"\nchar_table_{tt}:\n")
+        for lidx,_ in enumerate(lb):
+            layer_entry = f"{tt}_{lidx}_layer"
+            f.write(f"\t.long\t{layer_entry}\n")
+        for lidx,tile_list in enumerate(lb):
+            layer_entry = f"{tt}_{lidx}_layer"
+            f.write(f"{layer_entry}:\n")
+            for tidx,tile in enumerate(tile_list):
+                f.write("\t.long\t")
+                f.write(f"tile_{tt}_{lidx}_{tidx}" if tile else "0")
+                f.write("\n")
+            for tidx,tile in enumerate(tile_list):
+                if tile:
+                    f.write(f"tile_{tt}_{lidx}_{tidx}:\n")
+                    for t in tile:
+                        f.write(f"\t.long\t")
+                        if t < 0:
+                            f.write("0")
+                        else:
+                            f.write(f"tie_plane_{t}")
+                        f.write("\n")
+
+    f.write("\n* tile bitplanes\n")
+    # dump bitplanes
+    for k,v in tile_bitplane_cache.items():
+        f.write(f"tile_plane_{v}:")
+        bitplanelib.dump_asm_bytes(k,f,mit_format=True)
+
+    f.write("\n\t.section\t.datachip\n\n")
+
+
 
 
