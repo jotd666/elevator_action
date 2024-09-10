fc11d2d00f133442830ea1dad87371c8296aa5de
Jean-François Fabre <jotd666@gmail.com>
2024-09-11 00:05:12 +0200

    fixed intro sound

--- assets/amiga/convert_sounds.py
+++ assets/amiga/convert_sounds.py
@@ -32,7 +32,7 @@ sound_dict = {
 "FORGOT_DOCUMENTS_SND"               :{"index":0,"channel":0,"sample_rate":hq_sample_rate,"priority":5},
 "CREDIT_SND"               :{"index":1,"channel":0,"sample_rate":hq_sample_rate},
 "WALK_SND"                 :{"index":2,"channel":2,"sample_rate":hq_sample_rate,"priority":2},
-"HOOK_SHOT_SND"                :{"index":3,"pattern":0,"volume":40,'loops':False,"ticks":280},
+"HOOK_SHOT_SND"                :{"index":3,"pattern":0,"volume":30,'loops':False,"ticks":3200},
 "DOCUMENT_COLLECTED_SND"                :{"index":4,"channel":2,"sample_rate":hq_sample_rate,"priority":5},
 "RED_DOOR_OPENED_SND"                :{"index":5,"channel":3,"sample_rate":hq_sample_rate,"priority":5},
 "DOWN_THE_STAIRS_SND"                :{"index":6,"channel":2,"sample_rate":hq_sample_rate,"priority":5},
@@ -183,7 +183,7 @@ with open(sndfile,"w") as fst,open(outfile,"w") as fw:
     with open(os.path.join(sound_dir,f"{gamename}_conv.mod"),"rb") as f:
         contents = f.read()
     fw.write("{}:".format(music_module_label))
-    #write_asm(contents,fw)
+    write_asm(contents,fw)
     fw.write("\t.align\t8\n")
 
 
