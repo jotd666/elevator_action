bcf7f6265b2b1af044060d0aa3bdcd2203b1dbd3
Jean-François Fabre <jotd666@gmail.com>
2024-09-21 15:50:09 +0200

    more RE

--- src/aga/elevator_actionHD.s
+++ src/aga/elevator_actionHD.s
@@ -40,14 +40,14 @@ _config
 	dc.b	"C1:X:infinite lives:1;"
 	dc.b	"C1:X:no need to collect documents:2;"
 	dc.b	"C1:X:cheat keys:4;"
-	dc.b    "C2:L:level color:default,2,3,4;"
-*	dc.b    "C5:L:start lives:default,1,2,3,5;"
+	dc.b    "C2:L:start level color:default,2,3,4;"
 	dc.b	"C3:X:skip introduction:0;"
 	IFD		CHIP_ONLY
 	dc.b	"C3:X:break at startup:31;"
 	ENDC
 	dc.b    "C4:L:start floor:top,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,"
-	dc.b	"21,22,23,24,25,26,27,28,29"
+	dc.b	"21,22,23,24,25,26,27,28,29;"
+	dc.b	"C5:L:difficulty level:easiest,easy,normal,hard;"
 	dc.b	0
 
 	IFD BARFLY
